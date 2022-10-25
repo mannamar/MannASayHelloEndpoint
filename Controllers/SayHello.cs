6fb39ef7490ac51789069e18f0270d601a6a79ae
Amardeep Mann <amar2slick@gmail.com>
2022-10-25 15:23:26 -0700

    Comment header

--- Controllers/SayHello.cs
+++ Controllers/SayHello.cs
@@ -1,3 +1,9 @@
+// Amardeep Mann
+// 10-25-22
+// SayHello Endpoint
+// Added the ability to take in an input via web URL for our SayHello function
+// Peer reviewed by
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace MannASayHelloEndpoint.Controllers;
