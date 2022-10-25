f6b7c71ffc81b8218a47a3cecfdf6ffa94dd8a3c
Amardeep Mann <amar2slick@gmail.com>
2022-10-25 15:35:06 -0700

    Peer reviewed

--- Controllers/SayHello.cs
+++ Controllers/SayHello.cs
@@ -2,7 +2,7 @@
 // 10-25-22
 // SayHello Endpoint
 // Added the ability to take in an input via web URL for our SayHello function
-// Peer reviewed by
+// Peer reviewed by Arely: Amar has made a default string that pops up regardless if the user enter a input after the route name. Which hasn’t been taught yet so he had to go out of his way to learn this. Additionally, Amar has easy to understand name convention with the controller and route name. Overall great job and a lot of effort put in to get ahead.
 
 using Microsoft.AspNetCore.Mvc;
 
